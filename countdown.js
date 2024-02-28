6d195d1d9a5e6d75e236645ed302dbf28e35569d
olisagold <olisaejezie@gmail.com>
2024-02-27 17:36:26 -0800

    https://github.com/Olisagold/giveawaytimer.git

---
+++ countdown.js
@@ -0,0 +1,21 @@
+let endTime = new Date("Mar 12, 2024 11:30:00").getTime();
+
+let time = setInterval(() => {
+    let now = new Date().getTime();
+    let timeleft = endTime - now;
+
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math. floor((timeleft % (1000 * 60 * 60 * 24)) /
+    (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math. floor((timeleft % (1000 * 60)) / 1000);
+    let daysUI = document.querySelector(".days")
+    daysUI.innerHTML = days
+    let hoursUI = document.querySelector(".hours")
+    hoursUI.innerHTML = hours
+    let minutesUI = document .querySelector(".minutes")
+    minutesUI.innerHTML = minutes
+    let secondsUI = document.querySelector(".seconds")
+    secondsUI.innerHTML = seconds
+}, 1000)
+
